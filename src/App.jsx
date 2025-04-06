7ac5f91c3354cdf5686aa7794133b6660e7f4dca
rajarya199 <aryalrajan078@gmail.com>
2025-04-06 23:01:13 +0545

    fetch movie db

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,56 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import Search from './components/Search'
 
+const API_BASE_URL="https://api.themoviedb.org/3"
+const API_KEY=import.meta.env.VITE_TMDB_API_KEY
+
+const API_OPTIONS={
+  method:'GET',
+  headers:{
+    accept:'application/json',
+    Authorization:`Bearer ${API_KEY}`
+  }
+}
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('')
+  
+  const [movieList, setMovieList] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
+const fetchMovies=async()=>{
+  setIsLoading(true);
+  setErrorMessage('');
+
+  try{
+    const endpoint= `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+    const response= await fetch(endpoint,API_OPTIONS);
+    if(!response.ok) {
+      throw new Error('Failed to fetch movies');
+    }
+
+    const data = await response.json();
+    console.log(data)
+    if(data.Response==="False"){
+      setErrorMessage( data.Error ||"faile to fetch movies")
+      setMovieList([]);
+      return;
+    }
+    setMovieList(data.results ||[])
+  }
+  catch(error){
+    console.error(`Error fetching movies: ${error}`);
+      setErrorMessage('Error fetching movies. Please try again later.');
+  }
+  finally{
+    setIsLoading(false)
+  }
+
+}
+
+  useEffect(()=>{
+    fetchMovies()
+  },[])
   return (
     <main>
       <div className='pattern'/>
@@ -14,6 +61,26 @@ const App = () => {
         <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
 
         </header>
+        <section className='all-movies'>
+          <h2>All Movies</h2>
+{isLoading ?(
+  <p className='text-white'>Loading...</p>
+):errorMessage ?(
+  <p className="text-red-500">{errorMessage}</p>
+) :(
+  <ul>
+    {movieList.map((movie)=>(
+      <p className='text-white'>{movie.title}</p>
+    ))}
+  </ul>
+)
+}
+
+
+          {errorMessage &&  <p className="text-red-500">{errorMessage}</p>
+        }
+
+        </section>
       </div>
     </main>
   )
